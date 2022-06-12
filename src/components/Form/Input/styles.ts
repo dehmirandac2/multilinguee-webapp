5098d1e27eddcdd7ae03abec1760ec92f2c14660
Deborah Miranda Casanova Costa <dehmirandac2@Deborahs-MacBook-Pro.local>
2022-06-11 21:06:50 -0300

    add logout

--- src/components/Form/Input/styles.ts
+++ src/components/Form/Input/styles.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 import { TextField } from '@mui/material';
 
+type Prop = any;
+
 export const MainInput = styled(TextField)`
   && {
-    width: ${(full) => (full ? '100%' : 'calc(50% - 20px)')};
+    width: ${({ full }: Prop) => (full ? '100%' : 'calc(50% - 20px)')};
     margin-bottom: 20px;
     &:first-child {
       margin-right: 20px;
